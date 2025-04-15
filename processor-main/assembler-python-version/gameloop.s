10e3556133095bf048d59d8ad61c3cb3d4be4a38
Mathew Chu <mathew.chu@duke.edu>
2025-04-15 19:56:40 -0400

    it works?

--- processor-main/assembler-python-version/gameloop.s
+++ processor-main/assembler-python-version/gameloop.s
@@ -12,14 +12,14 @@ _update_player_pos:
     addi $a1, $zero, 511
     addi $a2, $zero, 25
     mul $ax, $ax, $a0
-    div $px, $ax, $a1
-    #add $px, $ax, $a2
+    div $ax, $ax, $a1
+    add $px, $ax, $a2
 
     # scale accel y data
     addi $a0, $zero, 429
     addi $a1, $zero, 511
     mul $ay, $ay, $a0
-    div $py, $ay, $a1
-    #add $py, $ay, $a2
+    div $ay, $ay, $a1
+    add $py, $ay, $a2
     
     j _update_player_pos
